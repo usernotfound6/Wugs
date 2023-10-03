2454637f6a5f7b2e603aec60244b2b59a98a2dec
Forrest Nelson <forrestnnelson@gmail.com>
2023-10-02 19:00:56 -0500

    updated interested form and POST route to add interested parties to a table

--- server/server.js
+++ server/server.js
@@ -10,6 +10,7 @@ const passport = require('./strategies/user.strategy');
 // Route includes
 const userRouter = require('./routes/user.router');
 const onboardingRouter = require('./routes/onboarding.router');
+const interestedRouter = require('./routes/interested.router');
 
 // Body parser middleware
 app.use(bodyParser.json());
@@ -25,6 +26,7 @@ app.use(passport.session());
 /* Routes */
 app.use('/api/user', userRouter);
 app.use('/api/onboarding', onboardingRouter);
+app.use('/api/interested', interestedRouter);
 
 // Serve static files
 app.use(express.static('build'));
