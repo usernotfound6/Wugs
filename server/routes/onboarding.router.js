b489fc79d11ae9a2741aa233294597ed66462497
LazyDotExe <papaporo@proton.me>
2023-10-03 09:27:49 -0500

    Finished demopraphic put router

--- server/routes/onboarding.router.js
+++ server/routes/onboarding.router.js
@@ -18,48 +18,25 @@ router.get("/", (req, res) => {
     });
 });
 
-/**
- * POST route template
- */
-router.post("/", (req, res) => {
+
+
+
+router.put("/demographic", (req, res) => {
   // POST route code here
 
-  let queryText = `INSERT INTO client (business_name,
-    address,
-    website,
-    manager_id,
-    phone,
-    hours_of_operation,
-    minimarket_location,
-    neighborhood_info,
-    demographics,
-    number_of_people,
-    target_age_group,
-    industry,
-    pictures,
-    contract,
-    admin_notes,
-    status_id)
-    VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16);
-    `;
+  let queryText = `UPDATE client
+  SET number_of_people = $1,
+  demographics = $2,
+  neighborhood_info = $3,
+  industry = $4
+  WHERE client.id = 16;`;
   pool
     .query(queryText, [
-      req.body.business_name,
-      req.body.address,
-      req.body.website,
-      req.body.manager_id,
-      req.body.phone,
-      req.body.hours_of_operation,
-      req.body.minimarket_location,
-      req.body.neighborhood_info,
-      req.body.demographics,
       req.body.number_of_people,
-      req.body.target_age_group,
+      req.body.demographics,
+      req.body.neighborhood_info,
       req.body.industry,
-      req.body.pictures,
-      req.body.contract,
-      req.body.admin_notes,
-      req.body.status_id,
+      
     ])
     .then((result) => {
       res.sendStatus(200);
@@ -70,43 +47,57 @@ router.post("/", (req, res) => {
     });
 });
 
-router.put("/:id", (req, res) => {
-  const client = req.body;
+// Service Choice router --------------------------------------
+router.put("/servicechoice", (req, res) => {
+  const client_id = req.body.client_id;
+  const service_id = req.body.service_id;
 
-  const queryText = `UPDATE client
-  SET
-    "business_name" = $1,
-    "address" = $2,
-    "website" = $3,
-    "manager_id" = $4,
-    "phone" = $5,
-    "hours_of_operation" = $6,
-    "minimarket_location" = $7,
-    "neighborhood_info" = $8,
-    "demographics" = $9,
-    "number_of_people" = $10,
-    "target_age_group" = $11,
-    "industry" = $12,
-    "pictures" = $13,
-    "contract" = $14,
-    "admin_notes" = $15,
-    "status_id" = $16,
+  const deleteQuery = `DELETE FROM client_service WHERE client_id = $1`;
+  const insertQuery = `INSERT INTO client_service (client_id, service_id) VALUES ($1, $2)`;
 
-  WHERE id = $17;`;
-
-  const queryValues = [
-    // Values go here
-  ];
+  const deleteValues = [client_id];
+  const insertValues = [client_id, service_id];
 
   pool
-    .query(queryText, queryValues)
+    .query(deleteQuery, deleteValues)
+    .then(() => {
+      return pool.query(insertQuery, insertValues);
+    })
     .then(() => {
       res.sendStatus(200);
     })
     .catch((err) => {
-      console.log("Error completing PUT client query", err);
+      console.log("Error completing PUT service query", err);
       res.sendStatus(500);
     });
 });
+// router.put("/servicechoice", (req, res) => {
+//   const client_id = req.body.client_id;
+//   const service_ids = req.body.service_id; // Assuming this is an array
+
+//   const deleteQuery = `DELETE FROM client_service WHERE client_id = $1`;
+//   const insertQuery = `INSERT INTO client_service (client_id, service_id) VALUES ($1, $2)`;
+
+//   const deleteValues = [client_id];
+  
+//   // Use Promise.all to handle multiple inserts
+//   Promise.all(
+//     service_ids.map((service_id) => {
+//       const insertValues = [client_id, service_id];
+//       return pool.query(deleteQuery, deleteValues)
+//         .then(() => pool.query(insertQuery, insertValues));
+//     })
+//   )
+//     .then(() => {
+//       res.sendStatus(200);
+//     })
+//     .catch((err) => {
+//       console.log("Error completing PUT service query", err);
+//       res.sendStatus(500);
+//     });
+// });
+
+
+
 
 module.exports = router;
