1bb02338dc851cc8cd50ce87eb98a1a0f1ba8e75
fraol <phrolaye@gmail.com>
2023-10-03 13:41:02 -0500

    get more info styling

--- src/components/GetMoreInfoPage/GetMoreInfoPage.jsx
+++ src/components/GetMoreInfoPage/GetMoreInfoPage.jsx
@@ -1,51 +1,77 @@
-import React, { useState } from 'react';
-import { useSelector } from 'react-redux';
-import axios from 'axios';
+import React, { useState } from "react";
+import { useSelector } from "react-redux";
+import axios from "axios";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import {
+  Button,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogActions,
+  DialogContentText,
+  Typography,
+} from "@mui/material";
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 
 // Path: /getmoreinfo
 
 function GetMoreInfoPage() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+  const [industry, setIndustry] = useState("");
+  const [aboutClient, setAboutClient] = useState("");
+  const [whyWugs, setWhyWugs] = useState("");
+  const [openConfirmation, setOpenConfirmation] = useState(false);
 
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [phone, setPhone] = useState('');
-  const [industry, setIndustry] = useState('');
-  const [aboutClient, setAboutClient] = useState('');
-  const [whyWugs, setWhyWugs] = useState('');
+  const history = useHistory();
 
   // captures characters into 3 groups, adding parentheses around first group and - between 2nd/3rd groups
   function getFormattedPhoneNum(input) {
     let output = "(";
-    input.replace(/^\D*(\d{0,3})\D*(\d{0,3})\D*(\d{0,4})/, function (match, g1, g2, g3) {
-      if (g1.length) {
-        output += g1;
-        if (g1.length == 3) {
-          output += ")";
-          if (g2.length) {
-            output += " " + g2;
-            if (g2.length == 3) {
-              output += " - ";
-              if (g3.length) {
-                output += g3;
+    input.replace(
+      /^\D*(\d{0,3})\D*(\d{0,3})\D*(\d{0,4})/,
+      function (match, g1, g2, g3) {
+        if (g1.length) {
+          output += g1;
+          if (g1.length == 3) {
+            output += ")";
+            if (g2.length) {
+              output += " " + g2;
+              if (g2.length == 3) {
+                output += " - ";
+                if (g3.length) {
+                  output += g3;
+                }
               }
             }
           }
         }
       }
-    }
     );
     return output;
   }
 
   // Function to format the phone number as you type
   const handleFormatPhoneNumber = (event) => {
-    const inputValue = event.target.value.replace(/\D/g, '');
+    const inputValue = event.target.value.replace(/\D/g, "");
     // Remove non-digit characters
     let formattedValue = getFormattedPhoneNum(inputValue);
     setPhone(formattedValue);
   };
 
+  const handleConfirmSubmit = () => {
+    setOpenConfirmation(false);
+    history.push("/home");
+  };
+
+  const handleCloseConfirmation = () => {
+    setOpenConfirmation(false);
+  };
+
   const handleSubmit = (event) => {
+    setOpenConfirmation(true);
     event.preventDefault();
 
     // console.log("inside handleSubmit");
@@ -55,98 +81,172 @@ function GetMoreInfoPage() {
       phone_number: phone,
       industry: industry,
       about_you: aboutClient,
-      why_wugs: whyWugs
-    }
-    console.log(getMoreInfoObject)
+      why_wugs: whyWugs,
+    };
+    console.log(getMoreInfoObject);
 
-    axios.post('/api/interested', getMoreInfoObject)
-      .then(response => {
+    axios
+      .post("/api/interested", getMoreInfoObject)
+      .then((response) => {
         console.log("success with interested party POST:", response);
       })
-      .catch(error => {
+      .catch((error) => {
         console.error("error with interested party POST:", error);
-      })
-    setName('');
-    setEmail('');
-    setPhone('');
-    setIndustry('');
-    setAboutClient('');
-    setWhyWugs('');
-  }
+      });
+    setName("");
+    setEmail("");
+    setPhone("");
+    setIndustry("");
+    setAboutClient("");
+    setWhyWugs("");
+  };
 
   return (
     <div>
-      <h2>Contact Wugs</h2>
-
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="name">Name:</label><br />
-        <input
-          type="text"
+      <h3 style={{ marginLeft: "12px" }}>Get In Touch With Us</h3>
+      <Box
+        component="form"
+        sx={{
+          "& > :not(style)": { m: 1, width: "25ch" },
+        }}
+        noValidate
+        autoComplete="off"
+      >
+        <TextField
           id="name"
-          name="name"
+          label="Name"
+          variant="outlined"
+          inputProps={{ style: { color: "beige" } }}
+          InputLabelProps={{ style: { color: "beige" } }}
+          type="text"
           placeholder="John Doe"
           value={name}
           onChange={(event) => setName(event.target.value)}
           required
-        /><br />
+        />
+        <br />
 
-        <label htmlFor="email">Email Address:</label><br />
-        <input
-          type="email"
+        <TextField
           id="email"
-          name="email"
+          label="Email Address"
+          variant="outlined"
+          inputProps={{ style: { color: "beige" } }}
+          InputLabelProps={{ style: { color: "beige" } }}
+          type="email"
           placeholder="john.doe@gmail.com"
           value={email}
           onChange={(event) => setEmail(event.target.value)}
           required
-        /><br />
+        />
+        <br />
 
-        <label htmlFor="phone">Phone Number:</label><br />
-        <input
-          type="tel"
+        <TextField
           id="phone"
-          name="phone"
+          label="Phone Number"
+          variant="outlined"
+          inputProps={{ style: { color: "beige" } }}
+          InputLabelProps={{ style: { color: "beige" } }}
+          type="tel"
           placeholder="(123) 456-7890"
           value={phone}
           onChange={handleFormatPhoneNumber}
           required
-        /><br />
+        />
+        <br />
 
-        <label htmlFor="industry">Business / Industry:</label><br />
-        <input
-          type="text"
+        <TextField
           id="industry"
-          name="industry"
+          label="Business / Industry"
+          variant="outlined"
+          inputProps={{ style: { color: "beige" } }}
+          InputLabelProps={{ style: { color: "beige" } }}
+          type="text"
           placeholder="School, Office, Warehouse, etc"
           value={industry}
           onChange={(event) => setIndustry(event.target.value)}
-        /><br />
+        />
+        <br />
 
-        <label htmlFor="about">About You:</label><br />
-        <textarea
+        <TextField
           id="aboutClient"
-          name="aboutClient"
-          value={aboutClient}
+          label="About You"
+          variant="outlined"
+          inputProps={{ style: { color: "beige" } }}
+          InputLabelProps={{ style: { color: "beige" } }}
+          multiline
+          rows={4}
           placeholder="Example: We operate a university library serving students and faculty."
+          value={aboutClient}
           onChange={(event) => setAboutClient(event.target.value)}
-          rows="4"
-          cols="50"
-        ></textarea><br />
+        />
+        <br />
 
-        <label htmlFor="whyWugs">What brings you to Wugs today?</label><br />
-        <textarea
+        <TextField
           id="whyWugs"
-          name="whyWugs"
-          value={whyWugs}
+          label="What Brings You To Wugs Today?"
+          variant="outlined"
+          style={{ width: 370 }}
+          inputProps={{ style: { color: "beige" } }}
+          InputLabelProps={{ style: { color: "beige" } }}
+          multiline
+          rows={6}
           placeholder="Example: Having a mini-market on-site would be a great convenience for our students and staff."
+          value={whyWugs}
           onChange={(event) => setWhyWugs(event.target.value)}
-          rows="4"
-          cols="50"
-        ></textarea><br />
+        />
+        <br />
+      </Box>
 
-        <button type="submit" value="Submit">Submit</button>
-      </form>
+      <Button
+        onClick={handleSubmit}
+        sx={{
+          marginTop: 1.5,
+          marginLeft: 2,
+          height: 50,
+          width: 120,
+          borderRadius: 1,
+        }}
+        color="success"
+        variant="contained"
+        autoFocus
+      >
+        Submit
+      </Button>
 
+      {/* Confirmation Dialog */}
+
+      <Dialog
+        open={openConfirmation}
+        onClose={handleCloseConfirmation}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+        PaperProps={{
+          style: {
+            background: "beige",
+          },
+        }}
+      >
+        <DialogTitle id="alert-dialog-title">
+          Thank You For Expressing Interest!
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            You will be emailed soon with follow-up information. Once you click
+            'Go Back' you will be redirected to the home page where you can get
+            started on the onboarding process.
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button
+            onClick={handleConfirmSubmit}
+            color="success"
+            variant="contained"
+            autoFocus
+          >
+            Go Back
+          </Button>
+        </DialogActions>
+      </Dialog>
     </div>
   );
 }
