27b418b7b9219f78b4c05c2b4776568342252e4c
LazyDotExe <papaporo@proton.me>
2023-10-05 22:44:41 -0500

    Changed the getAllClients saga to target api/admin to hit the Admin router.

--- src/components/AdminPage/AdminPage.jsx
+++ src/components/AdminPage/AdminPage.jsx
@@ -1,14 +1,27 @@
-import React, { useState } from 'react';
-import {useSelector} from 'react-redux';
+import React, { useState, useEffect } from 'react';
+import { useHistory } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
 
 // Basic functional component structure for React with default state
 // value setup. When making a new component be sure to replace the
 // component name Admin with the name for the new component.
 function AdminPage() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch({ type: 'FETCH_ALL_CLIENTS' });
+    // if (user.admin) {
+    //   dispatch({ type: 'FETCH_ALL_CLIENTS' })
+    // } else {
+    //   dispatch({ type: 'FETCH_CLIENT', payload: user.id })
+    // }
+  }, []);
   // Using hooks we're creating local state for a "heading" variable with
   // a default value of 'Functional Component'
   // const store = useSelector((store) => store);
   // const [heading, setHeading] = useState('Functional Component');
+    const allClients = useSelector((store) => store.allClients);
+    console.log("In Admin page", allClients)
 
   return (
     <div>
