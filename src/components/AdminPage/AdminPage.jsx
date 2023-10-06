76d39958c6f2ffc2ebb4467463c9ac392b2d190a
LazyDotExe <papaporo@proton.me>
2023-10-06 14:51:15 -0500

    cannot figure out how to pass the dispatch info to the sibling component

--- src/components/AdminPage/AdminPage.jsx
+++ src/components/AdminPage/AdminPage.jsx
@@ -8,9 +8,14 @@ import Typography from "@mui/material/Typography";
 import Modal from "@mui/material/Modal";
 import Columns from "./Columns";
 import BasicSelect from "./BasicSelect";
-import MultilineTextFields from "./MultiLineTextField";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
+import TextField from "@mui/material/TextField";
 
 function AdminPage() {
+  const dispatch = useDispatch();
   const [selectedRowId, setSelectedRowId] = useState(null);
   const admin = useSelector((store) => store.admin);
   console.log("In Admin page", admin);
@@ -62,8 +67,31 @@ function AdminPage() {
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
 
+  const [status, setStatus] = React.useState("");
+  const [input, setInput] = React.useState("");
+
+  console.log("inside status state", status);
+
+  const dropdown = (event) => {
+    setStatus(event.target.value);
+  };
+  console.log("MultilineTextField Input", input);
+
+  const inputField = (event) => {
+    setInput(event.target.value);
+  };
+
+  function editClient() {
+    dispatch({
+      // type: "SAVE_FAVORITE",
+      // payload: { id: recipe.recipe_id }, // Remember to put your payload in an object or your Saga/router will get mad
+    });
+  }
+
+  
+
   return (
-    <div style={{ height: 400, width: "100%" }}>
+    <div style={{ height: 400, width: "100%",}}>
       <DataGrid
         rows={rows}
         columns={columns}
@@ -80,8 +108,39 @@ function AdminPage() {
           <Typography id="modal-modal-title" variant="h6" component="h2">
             Text in a modal
           </Typography>
-          <BasicSelect/>
-          <MultilineTextFields/>
+          {/* <BasicSelect/> */}
+          <Box sx={{ minWidth: 120 }}>
+      <FormControl fullWidth>
+        <InputLabel id="demo-simple-select-label">Status</InputLabel>
+        <Select
+          labelId="demo-simple-select-label"
+          id="demo-simple-select"
+          value={status}
+          label="Status"
+          onChange={dropdown}
+        >
+          <MenuItem value={1}>Onboarding Incomplete</MenuItem>
+          <MenuItem value={2}>Pending Wugs Approval</MenuItem>
+          <MenuItem value={3}>Render In Progress</MenuItem>
+          <MenuItem value={4}>Contract Sent Awaiting Completion</MenuItem>
+          <MenuItem value={5}>Pending Contract Approval</MenuItem>
+          <MenuItem value={6}>Account Active</MenuItem>
+          <MenuItem value={7}>Account Inactive</MenuItem>
+        </Select>
+        <div style={{ marginTop: "20px" }}>
+          <TextField
+            id="filled-multiline-static"
+            label="Client Notes"
+            multiline
+            rows={4}
+            //   defaultValue="Default Value"
+            variant="filled"
+            onChange={inputField}
+          />
+        </div>
+      </FormControl>
+    </Box>
+          {/* <MultilineTextFields/> */}
           <Typography id="modal-modal-description" sx={{ mt: 2 }}>
             ID: {selectedRowId} {/* Display the selected row's ID */}
           </Typography>
