3a29e8b339da9b565dd4aaf92d63f357dace9d4a
Peter Ingles <peter.ingles@gmail.com>
2023-10-07 15:50:15 -0500

    Products maopped to image buttons on FoodPreferencesPage

--- src/redux/reducers/products.reducer.js
+++ src/redux/reducers/products.reducer.js
@@ -1,10 +1,8 @@
-// import { combineReducers } from "redux";
-
-const singleClient = (state = {}, action) => {
+const allProducts = (state = {}, action) => {
     if (action.type === 'SET_ALL_PRODUCTS') {
         return action.payload;
     }
     return state;
 }
 
-export default singleClient;
+export default allProducts;
