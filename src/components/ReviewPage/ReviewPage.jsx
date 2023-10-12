8d1ccfee1aa65c698c8cca75b2e2aca9f78135bc
Peter Ingles <peter.ingles@gmail.com>
2023-10-12 14:29:51 -0500

    Embedded Calendly widget

--- src/components/ReviewPage/ReviewPage.jsx
+++ src/components/ReviewPage/ReviewPage.jsx
@@ -18,47 +18,43 @@ import { PopupWidget } from "react-calendly";
 
 // Path: /clientstatus
 
-
-
 const theme = createTheme({
   palette: {
     mode: "dark", // or 'dark' for the dark theme
   },
 });
 
-
 function ReviewPage() {
-  // Using hooks we're creating local state for a "heading" variable with
-  // a default value of 'Functional Component'
-  // const store = useSelector((store) => store);
-  // const [heading, setHeading] = useState('Functional Component');
   const client = useSelector((store) => store.client);
   const user = useSelector((store) => store.user);
   const history = useHistory();
   const rootElement = document.getElementById("popup-root");
 
   const handleSubmit1 = () => {
-    history.push('/servicechoice')
+    history.push('/servicechoice');
   }
   const handleSubmit2 = () => {
-    history.push('/foodpreferences')
+    history.push('/foodpreferences');
   }
   const handleSubmit3 = () => {
-    history.push('/demographics')
+    history.push('/demographics');
   }
   const handleSubmit4 = () => {
-    history.push('/clientlocationinfo')
+    history.push('/clientlocationinfo');
   }
 
+  const openCalendlyLink = () => {
+    Calendly.initPopupWidget({ url: "https://calendly.com/dontyellwillcry" });
+  };
 
   return (
     <ThemeProvider theme={theme}>
       <div>
-      <MyStepper step={5} />
-         <CssBaseline />
+        <MyStepper step={5} />
+        <CssBaseline />
         <div style={{ textAlign: "center" }}>
-          <Typography variant= 'h4' marginTop={3}  style={{ color: "beige" }}>Review Submission</Typography>
-          </div>{" "}
+          <Typography variant='h4' marginTop={3} style={{ color: "beige" }}>Review Submission</Typography>
+        </div>
         <Container
           maxWidth="lg"
           sx={{
@@ -67,16 +63,15 @@ function ReviewPage() {
             // Add any other custom styles here
           }}
         >
-       
-
-          <div style={{ color: "beige" , textAlign: "center" }}><Typography variant= 'h6' marginTop={1} marginBottom={2} >Your Current Info With Wugs:</Typography></div>
-          <hr width={800} style={{ color: "beige" }}/>
+          <div style={{ color: "beige", textAlign: "center" }}>
+            <Typography variant='h6' marginTop={1} marginBottom={2}>Your Current Info With Wugs:</Typography>
+          </div>
+          <hr width={800} style={{ color: "beige" }} />
           <br />
-
           <Box>
             <Grid container spacing={1}>
               <Grid item xs={6} md={4}>
-                <Card variant="outlined" sx={{  borderRadius: 3, height: 270, backgroundColor: '#484747', boxShadow: 5 }}>
+                <Card variant="outlined" sx={{ borderRadius: 3, height: 270, backgroundColor: '#484747', boxShadow: 5 }}>
                   <CardContent>
                     <Typography
                       sx={{ fontSize: 24 }}
@@ -146,7 +141,7 @@ function ReviewPage() {
                     <Box textAlign={"right"}>
                       <Button onClick={handleSubmit4}
                         sx={{
-                         margin: 'auto',
+                          margin: 'auto',
                           height: 50,
                           width: 120,
                           borderRadius: 1,
@@ -162,7 +157,7 @@ function ReviewPage() {
                 </Card>
               </Grid>
               <Grid item xs={6} md={4}>
-                <Card variant="outlined" sx={{  borderRadius: 3, height: 270, backgroundColor: '#484747', boxShadow: 5 }}>
+                <Card variant="outlined" sx={{ borderRadius: 3, height: 270, backgroundColor: '#484747', boxShadow: 5 }}>
                   <CardContent>
                     <Typography
                       sx={{ fontSize: 24 }}
@@ -194,7 +189,7 @@ function ReviewPage() {
                 </Card>
               </Grid>
               <Grid item xs={6} md={4}>
-                <Card variant="outlined" sx={{  borderRadius: 3, height: 270, backgroundColor: '#484747', boxShadow: 5 }}>
+                <Card variant="outlined" sx={{ borderRadius: 3, height: 270, backgroundColor: '#484747', boxShadow: 5 }}>
                   <CardContent>
                     <Typography
                       sx={{ fontSize: 24 }}
@@ -229,40 +224,57 @@ function ReviewPage() {
                       >
                         Edit
                       </Button>
+                     
+                    </Box>
+                  </CardContent>
+                </Card>
+              </Grid>
+              <Grid item xs={6} md={4}>
+                <Card variant="outlined" sx={{ borderRadius: 3, height: 270, backgroundColor: '#484747', boxShadow: 5 }}>
+                  <CardContent>
+                    <Typography
+                      sx={{ fontSize: 24 }}
+                      color="beige"
+                      gutterBottom
+                    >
+                      Food Choices:
+                    </Typography>
+                    {client.product_types ?
+                      client.product_types.map((product, index) => (
+                        <Typography variant="h8" component="div" key={index}>
+                          {product}
+                        </Typography>
+                      ))
+                      :
+                      <Typography variant="h8" component="div">
+                        None Indicated
+                      </Typography>
+                    }
+                    <Box textAlign={"right"}>
+                      <Button onClick={openCalendlyLink}
+                        sx={{
+                          marginTop: 1.5,
+                          marginLeft: 2,
+                          height: 50,
+                          width: 120,
+                          borderRadius: 1,
+                        }}
+                        color="secondary"
+                        variant="contained"
+                        autoFocus
+                      >
+                        Schedule Meeting
+                      </Button>
+                     
                     </Box>
                   </CardContent>
                 </Card>
               </Grid>
             </Grid>
-            <PopupWidget
-          url="https://calendly.com/dontyellwillcry"
-          rootElement={rootElement}
-          text="Click here to schedule meeting!"
-          textColor="beige"
-          color="#00a2ff"
-          style={{
-            position: "absolute",
-            top: "50px",
-            left: "20px",
-            
-          }}
-        />
-            {/* </Box> */}
           </Box>
         </Container>
-        <PopupWidget
-          url="https://calendly.com/dontyellwillcry"
-          rootElement={rootElement}
-          text="Click here to schedule meeting!"
-          textColor="beige"
-          color="#00a2ff"
-          style={{
-            position: "absolute",
-            top: "50px",
-            left: "20px",
-            
-          }}
-        />
+        <Card>
+        </Card>
       </div>
     </ThemeProvider>
   );
