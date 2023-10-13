6bc24e10b76f61d741864fc324deab38d912c5d0
LazyDotExe <papaporo@proton.me>
2023-10-12 19:57:16 -0500

    Added the dummy data to the Service page. Also move the button to the right

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,47 +1,49 @@
-import React, { useEffect } from 'react';
+import React, { useEffect } from "react";
 import {
   HashRouter as Router,
   Redirect,
   Route,
   Switch,
-} from 'react-router-dom';
+} from "react-router-dom";
 
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch, useSelector } from "react-redux";
 
-import Nav from '../Nav/Nav';
-import Footer from '../Footer/Footer';
+import Nav from "../Nav/Nav";
+import Footer from "../Footer/Footer";
 
-import ProtectedRoute from '../ProtectedRoute/ProtectedRoute';
-import './App.css';
+import ProtectedRoute from "../ProtectedRoute/ProtectedRoute";
+import "./App.css";
 
 // all of our pages!
-import AboutPage from '../AboutPage/AboutPage';
-import UserPage from '../UserPage/UserPage';
-import InfoPage from '../InfoPage/InfoPage';
-import LandingPage from '../LandingPage/LandingPage';
-import LoginPage from '../LoginPage/LoginPage';
-import RegisterPage from '../RegisterPage/RegisterPage';
-import GetMoreInfoPage from '../GetMoreInfoPage/GetMoreInfoPage';
-import PrimingPage from '../PrimingPage/PrimingPage';
-import ServiceChoicePage from '../ServiceChoicePage/ServiceChoicePage';
-import ClientLocationInfoPage from '../ClientLocationInfoPage/ClientLocationInfoPage';
-import DemographicsPage from '../DemographicsPage/DemographicsPage';
-import FoodPreferencesPage from '../FoodPreferencesPage/FoodPreferencesPage';
-import AdditionalInfoPage from '../AdditionalInfoPage/AdditionalInfoPage';
-import ReviewPage from '../ReviewPage/ReviewPage';
-import AdminPage from '../AdminPage/AdminPage';
-import ClientStatusPage from '../ClientStatusPage/ClientStatusPage';
+import AboutPage from "../AboutPage/AboutPage";
+import UserPage from "../UserPage/UserPage";
+import InfoPage from "../InfoPage/InfoPage";
+import LandingPage from "../LandingPage/LandingPage";
+import LoginPage from "../LoginPage/LoginPage";
+import RegisterPage from "../RegisterPage/RegisterPage";
+import GetMoreInfoPage from "../GetMoreInfoPage/GetMoreInfoPage";
+import PrimingPage from "../PrimingPage/PrimingPage";
+import ServiceChoicePage from "../ServiceChoicePage/ServiceChoicePage";
+import ClientLocationInfoPage from "../ClientLocationInfoPage/ClientLocationInfoPage";
+import DemographicsPage from "../DemographicsPage/DemographicsPage";
+import FoodPreferencesPage from "../FoodPreferencesPage/FoodPreferencesPage";
+import AdditionalInfoPage from "../AdditionalInfoPage/AdditionalInfoPage";
+import ReviewPage from "../ReviewPage/ReviewPage";
+import AdminPage from "../AdminPage/AdminPage";
+import ClientStatusPage from "../ClientStatusPage/ClientStatusPage";
+
+
 
 function App() {
   const dispatch = useDispatch();
 
-  const user = useSelector(store => store.user);
+  const user = useSelector((store) => store.user);
 
   useEffect(() => {
-    dispatch({ type: 'FETCH_USER' });
+    dispatch({ type: "FETCH_USER" });
   }, [dispatch]);
 
-  console.log(user.id)
+  console.log(user.id);
 
   return (
     <Router>
@@ -68,18 +70,10 @@ function App() {
             <GetMoreInfoPage />
           </Route>
 
-
-
-
-
           {/* //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////// */}
 
-
-
-          {                                               /* PROTECTED */}
-
-
-
+          {/* PROTECTED */}
+          
 
           {/* For protected routes, the view could show one of several things on the same route.
             Visiting localhost:3000/user will show the UserPage if the user is logged in.
@@ -157,13 +151,14 @@ function App() {
             <ReviewPage />
           </ProtectedRoute>
 
-          <ProtectedRoute
+          {/* <ProtectedRoute
             // logged in shows AdminPage else shows LoginPage
             exact
             path="/admin"
           >
             <AdminPage />
-          </ProtectedRoute>
+          </ProtectedRoute> */}
+          
 
           <ProtectedRoute
             // logged in shows ClientStatusPage else shows LoginPage
@@ -172,57 +167,49 @@ function App() {
           >
             <ClientStatusPage />
           </ProtectedRoute>
+          <ProtectedRoute exact path="/admin">
+            {user.admin === true ? (
+              <AdminPage />
+            ) : (
+              <Redirect to="/user" />
+            )}
+          </ProtectedRoute>
 
-
-
-          {                                               /* PROTECTED */}
-
-
+          {/* PROTECTED */}
 
           {/* //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////// */}
 
-
-
-          <Route
-            exact
-            path="/login"
-          >
-            {user.id ?
-              // If the user is already logged in, 
+          <Route exact path="/login">
+            {user.id ? (
+              // If the user is already logged in,
               // redirect to the /user page
               <Redirect to="/user" />
-              :
+            ) : (
               // Otherwise, show the login page
               <LoginPage />
-            }
+            )}
           </Route>
 
-          <Route
-            exact
-            path="/registration"
-          >
-            {user.id ?
-              // If the user is already logged in, 
+          <Route exact path="/registration">
+            {user.id ? (
+              // If the user is already logged in,
               // redirect them to the /user page
               <Redirect to="/user" />
-              :
+            ) : (
               // Otherwise, show the registration page
               <RegisterPage />
-            }
+            )}
           </Route>
 
-          <Route
-            exact
-            path="/home"
-          >
-            {user.id ?
-              // If the user is already logged in, 
+          <Route exact path="/home">
+            {user.id ? (
+              // If the user is already logged in,
               // redirect them to the /user page
               <Redirect to="/user" />
-              :
+            ) : (
               // Otherwise, show the Landing page
               <LandingPage />
-            }
+            )}
           </Route>
 
           {/* If none of the other routes matched, we will show a 404. */}
